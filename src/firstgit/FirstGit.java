8633d2be5031eff5da0b26d5995cd6d3b3cacb56
athanas1 <athanas1@L11109042793.wctc.msft>
2016-08-30 16:16:50 -0500

    Revert "Added Animal Class and modified main class to include another message"
    This reverts commit 2973cd92b6914951d13e71d0b048227161c64e4d.

--- src/firstgit/FirstGit.java
+++ src/firstgit/FirstGit.java
@@ -15,9 +15,7 @@ public class FirstGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello Java");
-        
-        System.out.println("Hello Class");
+        System.out.println("Hello World");
         
     }
     
